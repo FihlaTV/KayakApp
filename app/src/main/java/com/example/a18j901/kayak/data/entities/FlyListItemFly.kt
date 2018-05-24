ba8f4b02c6a08bec447a76151cb83ea5f52af6f8
Alumno18 J901 <a18j901@campustls.local>
2018-05-24 11:57:17 -0500

    add hayakApp

---
+++ app/src/main/java/com/example/a18j901/kayak/data/entities/FlyListItemFly.kt
@@ -0,0 +1,18 @@
+package com.example.a18j901.kayak.data.entities
+
+import java.util.*
+
+class FlyListItemFly(
+        val originDate: Date,
+        val originCity: String,
+val destinationDate: Date,
+val destinationCity: String
+)
+{
+    fun getFlyDuration(): Long {
+        return destinationDate.time - originDate.time
+    }
+    fun getFlyLongerThanDay() : Int{
+        return ((destinationDate.time - originDate.time) / 60*60*24).toInt()
+    }
+}
---
+++ app/src/main/java/com/example/a18j901/kayak/ui/screens/FlyListActivity.kt
@@ -0,0 +1,13 @@
+package com.example.a18j901.kayak.ui.screens
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.example.a18j901.kayak.R
+
+class FlyListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_fly_list)
+    }
+}
